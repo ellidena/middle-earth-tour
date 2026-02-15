174e3e189fedd7ae4fbe0e29fcf15df02d2fb1c0
ellidena <85340254+ellidena@users.noreply.github.com>
2026-02-15 13:07:44 +0100

    Added test for listing attractions

---
+++ src/test/java/tourism/controller/TouristControllerTest.java
@@ -0,0 +1,45 @@
+package tourism.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import tourism.model.TouristAttraction;
+import tourism.service.TouristService;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(TouristController.class)
+public class TouristControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private TouristService service;
+
+    @Test
+    void shouldListAllAttractions() throws Exception {
+        List<TouristAttraction> mockList = List.of(
+                new TouristAttraction(
+                        "Rivendell",
+                        "Peaceful refuge",
+                        "Eriador",
+                        List.of("Elves"),
+                        0
+                ));
+
+        when(service.getAll()).thenReturn(mockList);
+
+        mockMvc.perform(get("/attractions"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("attractionlist"))
+                .andExpect(model().attribute("attractions", mockList));
+    }
+}
